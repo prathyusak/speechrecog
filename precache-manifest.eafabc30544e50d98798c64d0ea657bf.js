c0c208ec1aabf9249202155d769dbd9af37805c6
prathyusak <kandaprathyusha@gmail.com>
2020-11-06 16:31:17 -0700

    Updates

---
+++ precache-manifest.eafabc30544e50d98798c64d0ea657bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4263171df96663a9d4ff3d2d8dbf0ec7",
+    "url": "/speechrecog/index.html"
+  },
+  {
+    "revision": "9de990f702feea6dd4ef",
+    "url": "/speechrecog/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9a13774ab2718614c0e2",
+    "url": "/speechrecog/static/css/main.d3537154.chunk.css"
+  },
+  {
+    "revision": "9de990f702feea6dd4ef",
+    "url": "/speechrecog/static/js/2.c7069815.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speechrecog/static/js/2.c7069815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a13774ab2718614c0e2",
+    "url": "/speechrecog/static/js/main.1ab97239.chunk.js"
+  },
+  {
+    "revision": "839221859c874c0316df",
+    "url": "/speechrecog/static/js/runtime-main.81a5fb85.js"
+  }
+]);
